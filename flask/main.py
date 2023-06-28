1a8c82acf56e29ce0a7e8e21f6346b32de244b29
Phmarcio <phablomrciomrcio54@gmail.com>
2023-06-27 22:41:10 -0300

    adicionado endpoint no app flask

--- flask/main.py
+++ flask/main.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    
     return "<p>Hello, World!</p>"
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    print("Treinamento encerrado")
+    return '', 204
